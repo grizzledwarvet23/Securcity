1616f55dc53bb925c17b8f14ba6b2f4c5908c270
grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
2022-10-15 14:52:43 -0500

    wassup

--- App.js
+++ App.js
@@ -1,27 +1,84 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, } from 'react-native';
-import MapView from 'react-native-maps';
+import { Button, StyleSheet, Text, TouchableOpacity, View, } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 import { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { useState } from 'react';
 
 export default function App() {
+
+  // TESTING OUT MARKERS W LAT/LONG --> WILL CHANGE TO GRABBING USER COORDS LATER
+  const [coords, setCoords] = useState({
+    latitude: 37.7,
+    longitude: -122.4324,
+    latitudeDelta: 0.0922,
+    longitudeDelta: 0.0421,
+  });
+
+  // state = {
+  //   textValue: 'REPORT'
+  // }
+  // const createReport = () => {
+  //   //change the text of the touchable opacity calling this
+  //   this.setState({textValue: 'WASSUP'})
+  // };
+  
   return (
     <View style={styles.container}>
-      <Text>Welcome to Securcity!</Text>
+      <Text style={styles.titleText}>Welcome to Securcity!</Text>
       <MapView
-        style={{height: '50%', width: '100%'}}
+        style={styles.map}
+         region={coords}
         provider={PROVIDER_GOOGLE}
         showsUserLocation={true}
-      />
+      >
+        <Marker coordinate={coords} title="MARKER"/>
+      </MapView>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>REPORT</Text>
+      </TouchableOpacity>
       <StatusBar style="auto"/>
     </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    fontSize: 10,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  //create a style for the map
+  map: {
+    alignSelf: 'stretch',
+    height: '100%',
+    // width: '100%',
+  },
+  //create a style for the title text
+  titleText: {
+    fontSize: 20,
+    fontWeight: "bold"
+  },
+  //create a style for the button
+  button: {
+    backgroundColor: 'red',
+    color: 'white',
+    padding: 10,
+    margin: 10,
+    width: 180,
+    height: 50,
+    borderRadius: 10,
+    justifyContent: 'center',
+  },
+  //create a style for the button text
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+    fontWeight: "bold",
+    textAlign: 'center',
+    textAlignVertical: 'center',
+  }
+
 });
