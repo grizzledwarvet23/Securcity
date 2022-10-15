0cd4f155b0ee8398bb8d6bb782b369b13842aa8b
grizzledwarvet23 <72232563+grizzledwarvet23@users.noreply.github.com>
2022-10-15 13:26:23 -0500

    added google maps

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import MapView from 'react-native-maps';
+import { PROVIDER_GOOGLE } from 'react-native-maps';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Welcome to Securcity!</Text>
+      <MapView
+        style={{height: '50%', width: '100%'}}
+        provider={PROVIDER_GOOGLE}
+        showsUserLocation={true}
+      />
+      <StatusBar style="auto"/>
     </View>
   );
 }
